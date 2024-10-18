1892c95fd78c2b5917c708959f5974067e5e0dee
Jeffrey Enriquez <jeffrey.enriquez@newfold.com>
2024-10-17 20:20:30 -0400

    Fix typo

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,18 +5,18 @@ export default {
   content: ['./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
-      /*       fontFamily: {
+      fontFamily: {
         sans: [
-          'Inter"',
+          'Inter',
           'ui-sans-serif',
           'system-ui',
           'sans-serif',
-          'Apple Color Emoji"',
-          'Segoe UI Emoji"',
-          'Segoe UI Symbol"',
-          'Noto Color Emoji"',
+          'Apple Color Emoji',
+          'Segoe UI Emoji',
+          'Segoe UI Symbol',
+          'Noto Color Emoji',
         ],
-      }, */
+      },
       borderRadius: {
         lg: 'var(--radius)',
         md: 'calc(var(--radius) - 2px)',
